c6ce9b99ed4b95d6827ea621b4472c262552dbf7
tmconnect <thomas.meyer@dreihochzwo.de>
2015-10-07 11:00:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+ACF - Collapse Fields
+=====================
+
+If there are a lot of rows within a Repeater or Flexible Content Field you have to scroll a lot to go to the row you like to edit and it could be a pain to find right row.
+
+This is where ACF Collapse Fields comes in place. You can collapse one specific row (e.g. "o.k. this row is finally correct - I don't have to edit it") or collapse all together for a better overview or sorting.
+
+The plugin works with Repeater Fields in Row and Block mode and it works well in a Repeater within a Repeater Field.
+
+![ACF Collapse Fields with Repeater Fields](http://www.dreihochzwo.de/download/collapse-field-repeater.png)
+
+For the Flexible Content field ACF allows collapsing a row by clicking on the row header. But there is no posibility to open or collapse all rows by one click. The ACF Collapse Fields plugin solves this problem and add a button to collapse or expand all rows at once.
+
+![ACF Collapse Fields with Flexible Content Fields](http://www.dreihochzwo.de/download/collapse-field-flexible.png)
+
+Because of the changes ACF made with version 5.3.1 this plugin works only with ACF 5.3.1 or newer.
+
+
+### Compatibility
+
+This plugin needs the installation of ACF 5.3.1 or newer.
+
+### Changelog
+
+**1.0.0**
+* Initial commit
---
+++ acf-collapse-fields/acf-collapse-fields.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Plugin Name: Advanced Custom Fields: Collapse Fields
+ * Plugin URI: http://www.dreihochzwo.de
+ * Description: Provides a way to collapse and expand Repeater & Flexible Content fields in order to enable better sorting.
+ * Version: 1.0.0
+ * Author: Thomas Meyer
+ * Author URI: http://www.dreihochzwo.de
+ */
+
+define( 'ACF_COLLAPSER_VERSION', '1.0.0' );
+
+load_plugin_textdomain( 'acf-collapse-fields', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
+
+/* Load the javascript and CSS files on the ACF admin pages */
+// 11 helps take precedence over core styles
+add_action( 'acf/input/admin_enqueue_scripts', 'acf_collapse_fields_assets', 11 );
+function acf_collapse_fields_assets() {
+	$acf_version = acf()->settings['version'];
+	
+	if ( version_compare( $acf_version, '5.3.1', '>=' ) ) {
+		
+		wp_register_script( 
+			'acf-collapse-fields-admin-js',
+			esc_url( plugins_url( 'js/acf-collapse-fields-admin.js', __FILE__ ) ),
+			array( 'jquery' ),
+			ACF_COLLAPSER_VERSION
+		);
+
+		// Localize the script with new data
+		$translation_array = array(
+			'expandAll'			=> __( 'Expand All Elements', 'acf-collapse-fields' ),
+			'collapseAll'		=> __( 'Collapse All Elements', 'acf-collapse-fields' )
+		);
+		wp_localize_script( 'acf-collapse-fields-admin-js', 'collapsetranslation', $translation_array );
+
+		wp_enqueue_script('acf-collapse-fields-admin-js');
+
+		wp_enqueue_style(
+			'acf-collapse-fields_admin-css',
+			esc_url( plugins_url( 'css/acf-collapse-fields-admin.css', __FILE__ ) ),
+			false,
+			ACF_COLLAPSER_VERSION
+		);
+	}
+}
